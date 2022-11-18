64141735d56d87b2753772a3d1b3cef652b0f026
Axyl Pfeifle <pfeifledaniel@gmail.com>
2022-11-17 16:11:20 -0800

    Added notifications when adding to cart and and catch for a logged out user trying to add

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -20,6 +20,7 @@ export class ProductComponent implements OnInit {
   testProduct! : IProduct
   id! : number
   value: number = 1;
+  cartText = "";
 
 
   constructor(public ProductsService : ProductsService, private route : ActivatedRoute) {
@@ -35,7 +36,17 @@ export class ProductComponent implements OnInit {
 
   addToCart() : void{
     //this.ProductsService.addToCart(AuthService.getUser().id, this.testProduct.id, this.value)
-    this.ProductsService.addToCart(AuthService.getUser().id, this.id, this.value).subscribe(response => {console.log(response)});
+
+    if (AuthService.getUser() == null){
+      this.cartText = "Cannot add to cart when not logged in"
+    }
+
+    else{
+      this.ProductsService.addToCart(AuthService.getUser().id, this.id, this.value).subscribe(response => {console.log(response)});
+      this.cartText = `added ${this.value} to your cart`
+      console.log(this.cartText);
+    }
+    
   }
 
   consoleLog(text : string){
